c0154fe9bbf841d0b9f09c8049fe8a3fdb7bf8c1
Gabrielly Nolasco <genolasco075@gmail.com>
2022-01-24 18:02:39 -0500

    “Progress on Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -16,27 +16,27 @@ if len(single_chr) > (1):
 if len(single_chr) == (0):
     print("Error: Character must be a single character.")
     exit()
-number_matching_chr: int = 0
+count_instance: int = 0
 print("Searching for " + single_chr + " in " + five_chr_word)
 if single_chr == five_chr_word[0]:
     print("" + single_chr + " found at index 0")
-    number_matching_chr = number_matching_chr + 1
+    count_instance = count_instance + 1
 if single_chr == five_chr_word[1]: 
     print("" + single_chr + " found at index 1")
-    number_matching_chr = number_matching_chr + 1
+    count_instance = count_instance + 1
 if single_chr == five_chr_word[2]:
     print("" + single_chr + " found at index 2") 
-    number_matching_chr = number_matching_chr + 1
+    count_instance = count_instance + 1
 if single_chr == five_chr_word[3]:
     print("" + single_chr + " found at index 3")
-    number_matching_chr = number_matching_chr + 1
+    count_instance = count_instance + 1
 if single_chr == five_chr_word[4]:
     print("" + single_chr + " found at index 4")
-    number_matching_chr = number_matching_chr + 1
+    count_instance = count_instance + 1
 
-if number_matching_chr > (1):
-    print(str(number_matching_chr) + " instances of " + single_chr + " found in " + five_chr_word)
-if number_matching_chr == (1):
-    print(str(number_matching_chr) + " instance of " + single_chr + " found in " + five_chr_word)
+if count_instance > (1):
+    print(str(count_instance) + " instances of " + single_chr + " found in " + five_chr_word)
+if count_instance == (1):
+    print(str(count_instance) + " instance of " + single_chr + " found in " + five_chr_word)
 else:
     print("No instances of " + single_chr + " found in " + five_chr_word)
