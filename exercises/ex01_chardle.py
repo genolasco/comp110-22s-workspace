6369c4027ddb16c513a7d7d503a84f789cdb7144
Gabrielly Nolasco <genolasco075@gmail.com>
2022-01-24 17:23:30 -0500

    “Progress on Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,14 +2,14 @@
 
 __author__ = "730411655"
 
-five_chr_word: str = input("Enter a 5-character word:")
+five_chr_word: str = input("Enter a 5-character word: ")
 if len(five_chr_word) < (5):
     print("Error: Word must contain 5 characters")
     exit()
 if len(five_chr_word) > (5):
     print("Error: Word must contain 5 characters")
     exit()
-single_chr: str = input("Enter a single character:")
+single_chr: str = input("Enter a single character: ")
 if len(single_chr) > (1):
     print("Error: Character must be a single character.")
     exit()
@@ -17,7 +17,7 @@ if len(single_chr) == (0):
     print("Error: Character must be a single character.")
     exit()
 number_matching_chr: int = 0
-print("Searching for " + single_chr + " in " + five_chr_word + ".")
+print("Searching for " + single_chr + " in " + five_chr_word)
 if single_chr == five_chr_word[0]:
     print("" + single_chr + " found at index 0")
     number_matching_chr = number_matching_chr + 1
@@ -31,6 +31,12 @@ if single_chr == five_chr_word[3]:
     print("" + single_chr + " found at index 3")
     number_matching_chr = number_matching_chr + 1
 if single_chr == five_chr_word[4]:
-    print("" + single_chr + " is found at index 4")
+    print("" + single_chr + " found at index 4")
     number_matching_chr = number_matching_chr + 1
-print(str(number_matching_chr) + " instances of " + single_chr + " found in " + five_chr_word)
+
+if number_matching_chr > (1):
+    print(str(number_matching_chr) + " instances of " + single_chr + " found in " + five_chr_word)
+if number_matching_chr == (1):
+    print(str(number_matching_chr) + " instance of " + single_chr + " found in " + five_chr_word)
+else:
+    print("No instances of " + single_chr + " found in " + five_chr_word)
