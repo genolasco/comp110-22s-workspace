1dcb9e3d0051655f70079947ed17490b5e071f9b
Gabrielly Nolasco <genolasco075@gmail.com>
2022-03-22 21:04:51 -0400

    Exercise 7

--- exercises/ex07/data_utils.py
+++ exercises/ex07/data_utils.py
@@ -1,5 +1,89 @@
 """Dictionary related utility functions."""
 
-__author__ = ""
-
+__author__ = "730411655"
+from csv import DictReader
 # Define your functions below
+
+
+def read_csv_rows(nc_durham_2015_march_21_to_26: str) -> list[dict[str, str]]:
+    """Read the rows of a csv into a 'table'."""
+    result: list[dict[str, str]] = []
+
+    file_handle = open(nc_durham_2015_march_21_to_26, "r", encoding="utf8")
+    csv_reader = DictReader(file_handle)
+
+    for row in csv_reader:
+        result.append(row)
+    
+    file_handle.close()
+
+    return result
+
+
+def column_values(table: list[dict[str, str]], column: str) -> list[str]:
+    """Produce a list[str] of all vlaues in a single column."""
+    result: list[str] = []
+    for row in table:
+        item: str = row[column]
+        result.append(item)
+    return result 
+
+
+def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Transform a row-oriented table to a column-oriented table."""
+    result: dict[str, list[str]] = {}
+
+    first_row: dict[str, str] = row_table[0]
+    for column in first_row:
+        result[column] = column_values(row_table, column)
+
+    return result
+
+
+def head(N: dict[str, list[str]], count_rows: int) -> dict[str, list[str]]:
+    """Produces a new column table with just the first N rows of data of each column."""
+    result: dict[str, list[str]] = {}
+
+    for column in N:
+        values: list[str] = []
+        i: int = 0 
+        while i < count_rows:
+            values.append(N[column][i])
+        i = i + 1
+        
+    return result
+
+
+def select(a: dict[str, list[str]], name: list[str]) -> dict[str, list[str]]:
+    """Produce a column based table with only a specific subset of the original columns."""
+    result: dict[str, list[str]] = {}
+    for column in name:
+        result[column] = a[column]
+    return result
+
+
+def concat(a: dict[str, list[str]], b: dict[str, list[str]]) -> dict[str, list[str]]:
+    """Produce a new column tabel with 2 column tables combined."""
+    result: dict[str, list[str]] = {}
+    for column in a:
+        result[column] = a[column]
+    for column in b:
+        if column in result:
+            result[column] = b[column]
+        else:
+            result[column] = b[column]
+    return result
+    
+
+def count(counter: list[str]) -> dict[str, int]: 
+    """Given a list, will produce a dictionary with unique values assocaiated to the number of times that value appears."""
+    result: dict[str, int] = {}
+    for item in counter:
+        if item in result:
+            result[item] += 1
+        else:
+            result[item] += 1
+    return result
+
+
+    
